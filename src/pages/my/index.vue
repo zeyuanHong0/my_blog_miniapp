d2ebe5744ff627f782d9ac57c473a8a7b423f6c5
Alexander Smith <hongzeyuan0@gmail.com>
2026-02-23 19:46:59 +0800

    feat:添加页面和样式，更新依赖项，新增图标资源

---
+++ src/pages/my/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: ''
+}
+</script>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/pages/tags/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "",
+};  
+</script>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
