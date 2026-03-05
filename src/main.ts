5afdcb76872d1bc97d1c6b71efa2837c92af9d1f
Alexander Smith <hongzeyuan0@gmail.com>
2026-03-05 16:25:57 +0800

    feat: 添加 @tdesign/uniapp 依赖并引入主题样式

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
 import "./styles/common.scss";
+import '@tdesign/uniapp/common/style/theme/index.css';
 export function createApp() {
   const app = createSSRApp(App);
   return {
