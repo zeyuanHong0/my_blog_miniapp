a619546687f8b318bf0aee2acfa962684e675900
Alexander Smith <hongzeyuan0@gmail.com>
2026-03-10 17:37:04 +0800

    feat(towxml): 重构组件结构并支持标题查询
    - 新增 queryHeadings 方法用于获取标题节点信息
    - towxml 组件新增 getHeadings 接口
    - 优化 towxml 与 decode 模板结构并统一属性写法

--- src/static/towxml/decode.js
+++ src/static/towxml/decode.js
@@ -1,26 +1,37 @@
-const config = require('./config');
+const config = require("./config");
 
 Component({
-	options: {
-		styleIsolation: 'apply-shared'
-	},
-	properties: {
-		nodes: {
-			type: Object,
-			value: {}
-		}
-	},
-	lifetimes: {
-		attached: function () {
-			const _ts = this;
+  options: {
+    styleIsolation: "apply-shared",
+  },
+  properties: {
+    nodes: {
+      type: Object,
+      value: {},
+    },
+  },
+  lifetimes: {
+    attached: function () {
+      const _ts = this;
 
-			config.events.forEach(item => {
-				_ts['_' + item] = function (...arg) {
-					if (global._events && typeof global._events[item] === 'function') {
-						global._events[item](...arg);
-					}
-				};
-			});
-		}
-	}
-})
+      config.events.forEach((item) => {
+        _ts["_" + item] = function (...arg) {
+          if (global._events && typeof global._events[item] === "function") {
+            global._events[item](...arg);
+          }
+        };
+      });
+    },
+  },
+  methods: {
+    queryHeadings(callback) {
+      // 获取所有标题元素的信息，并通过回调返回
+      this.createSelectorQuery()
+        .selectAll(".h2w__h1, .h2w__h2, .h2w__h3, .h2w__h4, .h2w__h5, .h2w__h6")
+        .boundingClientRect((rects) => {
+          callback(rects);
+        })
+        .exec();
+    },
+  },
+});
--- src/static/towxml/towxml.js
+++ src/static/towxml/towxml.js
@@ -1,16 +1,23 @@
 Component({
-  options:{
-    styleIsolation:'shared'
+  options: {
+    styleIsolation: "shared",
   },
-  properties:{
-    nodes:{
-      type:Object,
-      value:{}
-    }
+  properties: {
+    nodes: {
+      type: Object,
+      value: {},
+    },
   },
-  data:{
-    someData:{
-      
-    }
-  }
-})
+  data: {
+    someData: {},
+  },
+  methods: {
+    // 暴露给外部查询的方法
+    getHeadings(callback) {
+      const child = this.selectComponent("#my-decode");
+      if (child && typeof child.queryHeadings === "function") {
+        child.queryHeadings(callback);
+      }
+    },
+  },
+});
