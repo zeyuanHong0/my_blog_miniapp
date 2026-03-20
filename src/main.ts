c69cf7644b26ee725564a3b8a19b8db807423705
Alexander Smith <hongzeyuan0@gmail.com>
2026-03-20 17:49:17 +0800

    chore(package): 添加 pinia 依赖

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
 import "./styles/common.scss";
-import '@tdesign/uniapp/common/style/theme/index.css';
+import "@tdesign/uniapp/common/style/theme/index.css";
+import * as Pinia from "pinia";
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(Pinia.createPinia());
   return {
     app,
+    Pinia,
   };
 }
